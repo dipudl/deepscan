6c4e6152aca0b18177d4a551e630375b4aa2656c
dipu <dipu234dahal@gmail.com>
2025-02-23 23:41:52 +0545

    update: notebooks for dataset generation and training model

--- backend/evaluation.c
+++ backend/evaluation.c
@@ -1,39 +1,9 @@
 #include <stdio.h>
-
-int subtract(int first, int second) {
-    return first - second;
-}
+#include <string.h>
 
 int main() {
-    int i, n;
-    int a = 5, b = 2, c, d=7;
+    int array[] = {1, 2, 3, 4}, size = 4, sum = 0;
 
-    // initialize first and second terms
-    int t1 = 0, t2 = 1;
-
-    // initialize the next term (3rd term)
-    int nextTerm = t1 + t2;
-
-    // get no. of terms from user
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-
-    // print the first two terms t1 and t2
-    printf("Fibonacci Series: %d, %d, ", t1, t2);
-
-    // print 3rd to nth terms
-    for (i = 3; i <= n; ++i) {
-        printf("%d, ", nextTerm);
-        t1 = t2;
-        t2 = nextTerm;
-        nextTerm = t1 + t2;
-    }
-
-    // Arithmetic operators
-    c = a / ((b * a) % b);
-    d = a * b + c * d;
-
-    subtract(b, a);
-
-    return 0;
+    for (int i = 0; i < size; i++)
+        sum = sum - array[i];
 }
